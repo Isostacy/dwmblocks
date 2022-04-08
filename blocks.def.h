be99bc9ddc4ea8a61222ea7281f9dfc9a8e7ab7d
Isostacy <eybosslow@gmail.com>
2022-04-07 21:00:15 -0700

    added brackets

--- blocks.def.h
+++ blocks.def.h
@@ -2,14 +2,14 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 /*	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0}, */
-	{" ", "pamixer --get-volume",					1,		0},
-	{"",   "playerstatus",						1,		0},
-	{" ", "uname -r",				          	30,		0},
-	{"",   "forecast",				          	30,		0},
-	{" ", "date '+%a %b %d, %Y'",					5,		0},
-	{" ", "date '+%I:%M %p'", 		 			5,		0},
+	{"", "echo [ $(pamixer --get-volume)]",					1,		0},
+	{"",   "echo [$(playerstatus)]",						1,		0},
+	{"", "echo [ $(uname -r)]",				          	30,		0},
+	{"",   "echo [$(forecast)]",				          	30,		0},
+	{"", "echo [ $(date '+%a %b %d, %Y')]",					5,		0},
+	{"", "echo [ $(date '+%I:%M %p')]", 		 			5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = " ";
 static unsigned int delimLen = 5;
